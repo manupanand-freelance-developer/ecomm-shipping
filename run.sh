fa446df774f186f380ee102f5b646c54c88463cf
manupanand <manupanandindianinvestor@gmail.com>
2025-06-03 15:10:28 +0530

    updated

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 # to make sure that environment variables are present before running image
 
 
-if [ -z "${CART_ENDPOINT}"]; then
+if [ -z "${CART_ENDPOINT}" ]; then
     echo Input CART_ENDPOINT is missing
     exit 1
 fi
 
 
-if [ -z "${DB_HOST}"]; then
+if [ -z "${DB_HOST}" ]; then
     echo Input DB_HOST is missing
     exit 1
 fi
